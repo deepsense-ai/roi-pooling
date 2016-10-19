d3ed0943031dbbf28acf53d9b3c43eb4df37dafe
Tomasz Grela <tomasz@tomasz-grela.intra.codilime.com>
2016-10-19 11:49:16 +0200

    Computing the outputs and gradient from ROI layer with tensorflow and caffe, has issues with sharing resources

--- tensorflow_custom/roi_pooling/test_compare_caffe.py
+++ tensorflow_custom/roi_pooling/test_compare_caffe.py
@@ -2,7 +2,13 @@ import unittest
 import tempfile
 import os
 import numpy as np
+
 import caffe
+GPU_ID = 0 # Switch between 0 and 1 depending on the GPU you want to use.
+caffe.set_mode_gpu()
+caffe.set_device(GPU_ID)
+
+
 import tensorflow as tf
 import numpy as np
 from roi_pooling_ops import roi_pooling
@@ -36,7 +42,6 @@ def roinet_file(pooled_w, pooled_h, n_rois):
         return f.name
     
 def ROI_caffe(x, rois, pooled_w, pooled_h):
-    #TODO: return the gradients also
     #TODO: maybe check the initial conditions for the ROIs, input etc.?
     
     net_file = roinet_file(pooled_w=pooled_w, pooled_h=pooled_h, n_rois=len(rois))
@@ -48,42 +53,41 @@ def ROI_caffe(x, rois, pooled_w, pooled_h):
     net.forward()
     output = net.blobs['output'].data
     
-    #TODO: compute the gradient 
-    dummy_grad = np.zeros_like(x) #
-    return output, dummy_grad
+    net.blobs['output'].diff[...] = 1
+    net.backward()
+    caffe_grad = net.blobs['data'].diff
+    del net
+    return output, caffe_grad
 
 def ROI_tensorflow(x_input, rois_input, pooled_w, pooled_h):
     input = tf.placeholder(tf.float32)
     rois = tf.placeholder(tf.int32)
     
     y = roi_pooling(input, rois, pool_height=2, pool_width=2)
-    mean = tf.reduce_mean(y)
+    #mean = tf.reduce_mean(y)
+    mean = tf.reduce_sum(y)
     grads = tf.gradients(mean, input)
     with tf.Session('') as sess:
-        y_output =  sess.run(y, feed_dict={input: x_input, rois: rois_input})
-        grads_output = sess.run(grads, feed_dict={input: x_input, rois: rois_input})
+        with tf.device('/gpu:3'):
+            y_output =  sess.run(y, feed_dict={input: x_input, rois: rois_input})
+            grads_output = sess.run(grads, feed_dict={input: x_input, rois: rois_input})
     return y_output, grads_output
-    
+
 x = np.arange(20).reshape(1, 1, 4, 5)
 rois = [[0, 0, 0, 1, 1],
         [0, 0, 0, 3, 3],
         [0, 2, 2, 3, 3],
         [0, 0, 0, 4, 3]]
 rois = np.array(rois)
-tf_y, tf_grad = ROI_tensorflow(x, rois, 2,2)
-caffe_y, caffe_grad = ROI_caffe(x, rois, 2,2) 
 
-#    def test_backward(self):
-#        x = 7
-#        self.net.blobs['three'].diff[...] = x
-#        self.net.backward()
-#        for y in self.net.blobs['data'].diff.flat:
-#            self.assertEqual(y, 10**3 * x)
-#
-#    def test_reshape(self):
-#        s = 4
-#        self.net.blobs['data'].reshape(s, s, s, s)
-#        self.net.forward()
-#        for blob in self.net.blobs.itervalues():
-#            for d in blob.data.shape:
-#                self.assertEqual(s, d)
+if False:
+    caffe_y, caffe_grad = ROI_caffe(x, rois, 2, 2)
+    tf_y, tf_grad = ROI_tensorflow(x, rois, 2, 2)
+    
+    print('======== ROI outputs: ==========')
+    print('tf output: ', tf_y)
+    print('caffe output: ', caffe_y)
+    
+    printprint('======== ROI gradients: ==========')
+    print('tf output: ', tf_grad)
+    print('caffe output: ', caffe_grad)
