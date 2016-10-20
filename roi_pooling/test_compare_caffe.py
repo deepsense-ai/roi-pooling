b95539eddbb791a27be13be490d1af8f0286d65e
Tomasz Grel <tomasz.grel@codilime.com>
2016-10-20 10:03:57 +0200

    ROI pooling randomized test

--- roi_pooling/test_compare_caffe.py
+++ roi_pooling/test_compare_caffe.py
@@ -13,7 +13,7 @@ def ROI_tensorflow(x_input, rois_input, pooled_w, pooled_h):
     input = tf.placeholder(tf.float32)
     rois = tf.placeholder(tf.int32)
     
-    y = roi_pooling(input, rois, pool_height=2, pool_width=2)
+    y = roi_pooling(input, rois, pool_height=pooled_w, pool_width=pooled_h)
     mean = tf.reduce_sum(y)
     grads = tf.gradients(mean, input)
     with tf.Session('') as sess:
@@ -27,8 +27,33 @@ def ROI_tensorflow(x_input, rois_input, pooled_w, pooled_h):
 class RoiReferenceCaffeTest(unittest.TestCase): 
     def test_equal_to_caffe(self):
         (x, rois, caffe_y, caffe_grad) = load_data()
-        tf_y, tf_grad = ROI_tensorflow(x, rois, 2, 2)
+        
+        self.assertEqual(x.ndim, 4)
+        self.assertEqual(x.shape[0], 1)
+        self.assertEqual(x.shape[1], 1)
+        
+        self.assertEqual(rois.ndim, 2)
+        self.assertEqual(rois.shape[1], 5)
+        
+        p_width = 7
+        p_height = 7
+        tf_y, tf_grad = ROI_tensorflow(x, rois, p_width, p_height)
+
+        self.assertEqual(tf_grad.ndim, 4)
+        self.assertEqual(tf_grad.shape[0], 1)
+        self.assertEqual(tf_grad.shape[1], 1)
+        
+        self.assertEqual(tf_grad.shape[2], x.shape[2])
+        self.assertEqual(tf_grad.shape[3], x.shape[3])
+        
+        self.assertEqual(tf_y.ndim, 4)
+        self.assertEqual(tf_y.shape[1], 1)
+        self.assertEqual(tf_y.shape[2], p_width)
+        self.assertEqual(tf_y.shape[3], p_height)
             
+        self.assertEqual(tf_y.shape[0], rois.shape[0])
+        self.assertEqual(tf_grad.sum(), caffe_grad.sum())
+                
         print '======== ROI outputs: =========='
         print 'tf output: \n', tf_y
         print 'caffe output: \n', caffe_y
@@ -36,6 +61,10 @@ class RoiReferenceCaffeTest(unittest.TestCase):
         print '======== ROI gradients: =========='
         print 'tf grad: \n', tf_grad
         print 'caffe grad: \n', caffe_grad
+        
+        print 'tf grad sum: ', tf_grad.sum()
+        print 'caffe grad sum: ', caffe_grad.sum()
+        
         
         np.testing.assert_almost_equal(actual=tf_y, desired=caffe_y, decimal=10)
         np.testing.assert_almost_equal(actual=tf_grad, desired=caffe_grad, decimal=10)
