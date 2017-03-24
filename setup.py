94d8309d06b5e25480997140ad1ea89581eb15f7
Tomasz Grel <tomasz.grel@pascal01.intra.codilime.com>
2017-03-24 17:12:49 +0100

    Corrected installation

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
 from distutils.core import setup
 from distutils.command.install import install as DistutilsInstall
-
+import sys
 import subprocess
+
+try:
+    import tensorflow
+except ImportError:
+    print("Please install tensorflow 0.12.0 or later")
+    sys.exit()
+    
 
 class MyInstall(DistutilsInstall):
     def run(self):
@@ -18,5 +26,7 @@ setup(name='roi-pooling',
             package_data={'roi_pooling': ['roi_pooling.so']},
             cmdclass={'install': MyInstall}
 )
+
+    
 
 
